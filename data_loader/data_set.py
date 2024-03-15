ca278ff3cd5c28066b750bf3e9b35ec1f3209585
wueh <wueh@xiaopeng.com>
2024-03-15 17:43:08 +0800

    增加ETH-UCY数据集内容

---
+++ data_loader/data_set.py
@@ -0,0 +1,12 @@
+import os
+import math
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class TrajectoryDataset(Dataset):
+    def __init__():
+
+    def __len__(slef):
+
+    def __getitem__(self):
